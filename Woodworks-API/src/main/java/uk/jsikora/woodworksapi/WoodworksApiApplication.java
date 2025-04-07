9579b9abac6acdb9b1cd26d705153f36dba7939d
jsikora <grawires@gmail.com>
2025-04-07 10:37:34 +0200

    Init

---
+++ Woodworks-API/src/main/java/uk/jsikora/woodworksapi/WoodworksApiApplication.java
@@ -0,0 +1,13 @@
+package uk.jsikora.woodworksapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WoodworksApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WoodworksApiApplication.class, args);
+	}
+
+}
---
+++ Woodworks-API/src/test/java/uk/jsikora/woodworksapi/WoodworksApiApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.jsikora.woodworksapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WoodworksApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
