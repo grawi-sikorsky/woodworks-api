d88f456e6c4dcce6bd470c79affcd388b90aac8c
jsikora <grawires@gmail.com>
2025-04-10 21:55:34 +0200

    cors update for frontend

--- src/main/java/uk/jsikora/woodworksapi/security/SecurityConfig.java
+++ src/main/java/uk/jsikora/woodworksapi/security/SecurityConfig.java
@@ -5,6 +5,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -13,6 +16,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf(csrf -> csrf.disable())
+            .cors(cors -> cors.configurationSource(corsConfigurationSource())) // Włącz CORS
             .authorizeHttpRequests(auth -> auth.requestMatchers("/", "/public", "/response", "/items", "/login","/logout", "/css/**", "/js/**").permitAll()
                                                .anyRequest().authenticated()
                                   )
@@ -23,5 +27,18 @@ public class SecurityConfig {
                                     .clearAuthentication(true)
                                     .deleteCookies("JSESSIONID"));
         return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedOrigin("http://localhost:4200"); // Zezwól na Angular
+        configuration.addAllowedMethod("*"); // Zezwól na wszystkie metody (GET, POST, itp.)
+        configuration.addAllowedHeader("*"); // Zezwól na wszystkie nagłówki
+        configuration.setAllowCredentials(true); // Jeśli używasz ciasteczek/autoryzacji
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration); // Zastosuj do wszystkich endpointów
+        return source;
     }
 }
